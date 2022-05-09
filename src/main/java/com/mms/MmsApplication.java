3e31b7497eefa2121f78fe42e6d8836811b6ab9a
wangsungtaek <tjdxorv@naver.com>
2022-05-09 23:57:01 +0900

    POM 파일변경

--- src/main/java/com/mms/MmsApplication.java
+++ src/main/java/com/mms/MmsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MmsApplication extends SpringBootServletInitializer {
+public class MmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MmsApplication.class, args);
